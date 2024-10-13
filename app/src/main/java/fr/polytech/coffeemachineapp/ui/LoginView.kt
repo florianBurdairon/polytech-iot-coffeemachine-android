dd95cd6d82c8dfd475fdf23f31fbb9fbe6aa1614
florianBurdairon <florian.burdairon@gmail.com>
2024-10-14 00:12:42 +0200

    fix scroll on LoginView

--- app/src/main/java/fr/polytech/coffeemachineapp/ui/LoginView.kt
+++ app/src/main/java/fr/polytech/coffeemachineapp/ui/LoginView.kt
@@ -13,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
@@ -69,7 +71,9 @@ fun LoginView(navigator: DestinationsNavigator, snackbarHostState: SnackbarHostS
     )
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
